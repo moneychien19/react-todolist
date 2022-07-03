b3ffc4a97ed2ad9a7190780d8e7ae2d40728512d
lynn.chien <lynn.chien@bridgewell.com>
2022-07-03 15:14:46 +0800

    config: init eslint, install UI packages, test coding in App

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [2, 2],
+    'quotes': [2, 'single'],
+    'semi': [2, 'never']
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+import { Typography, Divider } from '@mui/material'
+
+const Wrapper = styled.div`
+  margin-top: 2rem;
+`
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 1rem 0;
+`
+
+const StyledDivider = styled(Divider)`
+  width: 100%;
+  background-color: #1F4690;
+  height: 0.2rem;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Header>
+          <Typography variant="h3" fontWeight={200}>4 July, Monday</Typography>
+          <StyledDivider variant="middle" />
+        </Header>
+      </Wrapper>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
